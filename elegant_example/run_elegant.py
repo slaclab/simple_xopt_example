850441a3a0c921219cc28845d68b1c0030056dc5
lee-edelen <edelen@slac.stanford.edu>
2020-11-11 16:46:13 -0800

    cleanup

--- elegant_example/run_elegant.py
+++ elegant_example/run_elegant.py
@@ -1,9 +1,9 @@
 import sys
-lume_path = ''
-sys.path.append(lume_path + 'lume-impact')
-sys.path.append(lume_path + 'openPMD-beamphysics')
-sys.path.append(lume_path + 'distgen')
-sys.path.append(lume_path + 'xopt')
+#lume_path = ''
+#sys.path.append(lume_path + 'lume-impact')
+#sys.path.append(lume_path + 'openPMD-beamphysics')
+#sys.path.append(lume_path + 'distgen')
+#sys.path.append(lume_path + 'xopt')
 
 
 import numpy as np
@@ -13,95 +13,51 @@ from pmd_beamphysics.plot import marginal_plot
 from pmd_beamphysics import ParticleGroup
 from pmd_beamphysics.plot import slice_statistics
 import tempfile
-from hashlib import blake2b
 import json
 import shutil
 import os
 import h5py
 from h5py import File
 import re
-
 from xopt.tools import fingerprint
 
-#ELEGANT_BIN='/global/cfs/cdirs/m669/aliaksei/elegant2020_rhel7/oag/apps/bin/linux-x86_64/elegant'
-#HDF5_BIN= '/global/cfs/cdirs/m669/aliaksei/elegant2020_rhel7/epics/extensions/bin/linux-x86_64/sdds2hdf'
-#H5_SAVE= './output/beams/'
 
-#class NpEncoder(json.JSONEncoder):
-#    """
-#    See: https://stackoverflow.com/questions/50916422/python-typeerror-object-of-type-int64-is-not-json-serializable/50916741
-#    """
-#    def default(self, obj):
-#        if isinstance(obj, np.integer):
-#            return int(obj)
-#        elif isinstance(obj, np.floating):
-#            return float(obj)
-#        elif isinstance(obj, np.ndarray):
-#            return obj.tolist()
-#        else:
-#            return super(NpEncoder, self).default(obj)
-#
-#def fingerprint(keyed_data, digest_size=16):
-#    """
-#    Creates a cryptographic fingerprint from keyed data. 
-#    Used JSON dumps to form strings, and the blake2b algorithm to hash.
-#    
-#    """
-#    h = blake2b(digest_size=16)
-#    for key in sorted(keyed_data.keys()):
-#        val = keyed_data[key]
-#        s = json.dumps(val, sort_keys=True, cls=NpEncoder).encode()
-#        h.update(s)
-#    return h.hexdigest()
-#
-
-def replaceTextBetween(originalText, delimeterA, delimterB, replacementText):
-    leadingText = originalText.split(delimeterA)[0]
-    trailingText = originalText.split(delimterB)[1]
-
-    return leadingText + delimeterA + replacementText + delimterB + trailingText
-
-def execute_elegant(settings, elename = 'LCLS2cuH.ele', ltename = 'LCLS2cuH.lte'):
-
-    #if 'timeout' in settings:
-    #    timeout = settings['timeout']
-    #else:
-    #    timeout = None
+def execute_elegant(settings, ele_fname = None, lte_fname = None):
     
-    ####-----grab other relevant info for run from settings-----
-    ####-----pop ones that aren't used at the moment too so are not in settings1 -- can fix later
+    ####---------#### grab other relevant info for run from settings
+    ####---------#### pop ones that aren't used at the moment too so are not in settings1 -- can fix later
     settings1 = settings.copy()
-    elename = settings1.pop('elename')
-    ltename = settings1.pop('ltename')
+    ele_fname = settings1.pop('ele_fname')
+    lte_fname = settings1.pop('lte_fname')
     path_search = settings1.pop('path_search')
     ELEGANT_BIN = settings1.pop('ELEGANT_BIN')
     HDF5_BIN = settings1.pop('HDF5_BIN')
-    H5_SAVE= settings1.pop('HDF5_BIN')
+    H5_SAVE = settings1.pop('H5_SAVE')
 
     finput_name = settings1.pop('finput_name')
     foutput_name = settings1.pop('foutput_name')
 
-    ####-----temp dir for run-----
+    ####---------#### temp dir for run
     tdir = tempfile.TemporaryDirectory()
     path = tdir.name
 
-    shutil.copy('run'+elename, path)
-    shutil.copy(ltename, path)
+    shutil.copy('run'+ele_fname, path)
+    shutil.copy(lte_fname, path)
     
-    ####-----run elegant-----
+    ####---------#### run elegant
 
     binname = ELEGANT_BIN
-    cmd = binname + ' ' + 'run' + elename 
+    cmd = binname + ' ' + 'run' + ele_fname 
     
     for s in settings1:
         cmd += ' -macro=' + s + '=' + str(settings1[s]) + ' '
-
-    print (cmd)
+        
     cmd_run = cmd.split()
     output = {'error':True, 'log':''}
 
-
     p = subprocess.run(cmd_run, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=path)
+    
+    ####---------#### process output
     PF = process_output(path,HDF5_BIN,finput_name,foutput_name)
     output['log'] = p.stdout
     output['error'] = False
@@ -112,12 +68,8 @@ def process_output(path, HDF5_BIN, finput_name='HXRSTART.out', foutput_name='HXR
 
     finput = os.path.join(path,finput_name)
     foutput = os.path.join(path,foutput_name)
-    
-    #init_dir = os.getcwd()
-    #os.chdir(path)
 
     cmd = f'{HDF5_BIN} {finput} {foutput}'
-    #print(cmd)
     cmd_run = cmd.split()
     output = {'error':True, 'log':''}
 
@@ -130,11 +82,6 @@ def process_output(path, HDF5_BIN, finput_name='HXRSTART.out', foutput_name='HXR
     
     return PF
 
-def run_elegant(settings):
-
-    PF = execute_elegant(settings)
-
-    return PF
 
 def calculate_core_pars(P, n_slice=50, n_slice_av=10):
     
@@ -168,70 +115,56 @@ def merit1(P):
 
 def evaluate_elegant(settings, a=1):
     
-    
-    ####-----grab other relevant info for run from settings-----
+    ####---------#### grab other relevant info for run from settings
     settings1 = settings.copy()
-    elename = settings1.pop('elename')
-    ltename = settings1.pop('ltename')
+    ele_fname = settings1.pop('ele_fname')
+    lte_fname = settings1.pop('lte_fname')
     path_search = settings1.pop('path_search')
     HDF5_BIN = settings1.pop('HDF5_BIN')
     H5_SAVE = settings1.pop('H5_SAVE')
     
-    ####-----adjust search path according to YAML and rewrite .ele as 'run<elename>'------    
+    ####---------#### adjust search path according to YAML and rewrite .ele as 'run<ele_fname>' 
     
-    f = open(elename,'r')
+    with open(ele_fname,'r') as f:
     
-    outlines=[]
-    
-    for line in f.readlines():
+        outlines=[]
 
-        x = line.strip()
-        
-        if x.startswith('search_path'):
-            line = ' search_path = "'+path_search+'"\n'
-            
-        outlines.append(line)
+        for line in f.readlines():
+
+            x = line.strip()
+
+            if x.startswith('search_path'):
+                line = ' search_path = "'+path_search+'"\n'
+
+            outlines.append(line)
             
     
-    f.close()
-    
-    with open('run'+elename, 'w') as f:
+    with open('run'+ele_fname, 'w') as f:
         for line in outlines:
             f.write(line)
             
-    f.close()
     
+    ####---------#### run elegant
     
-    ####-----run elegant-----
+    PF = execute_elegant(settings)
     
-
-    PF = run_elegant(settings)
-    
-    ####-----save h5 and archive----
+    ####---------#### save h5 and archive
     
     fnameh5 = 'elegant_sim_' + fingerprint(settings) + '.h5'
     PF.write(H5_SAVE+fnameh5)
-    
+
     output = merit1(PF)
     output['archive']=H5_SAVE+fnameh5
  
     return output
 
 
-def main(PATH=''):
+if __name__ == 'main':
 
     settings = {'L1_9_50_phase':50.5, 'L1_9_25_phase':50.5, 'L1_10_25_phase':50.5, 'X1_Xband_phase':-70, 
                 'L2_10_50_phase':55.5, 'L2_10_25_phase':55.5, 'L3_10_25_volt':1.6628471874e7, 'X_MAX':1.3e-3,             
                 'DX':0.9e-3, 'DP': 15.0e-5,
-                'INPUT_FILE':PATH+'elegant_particles.txt'}
+                'INPUT_FILE':'elegant_particles.txt'}
     
-    PF = run_elegant(settings)
-    #marginal_plot(PF, 't', 'pz')
+    PF = execute_elegant(settings)
 
-    #d = evaluate_elegant(settings)
-    #print (d)
-    
-    return 0
-    
-    
-#main()
